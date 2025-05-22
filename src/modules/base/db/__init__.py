ff83a2ee1984a75c3b833dc91be6fe3a16d5bfe3
Amit (Eddy) <ellaisys+github@gmail.com>
2025-05-22 19:49:12 +0530

    chore: alembic changes working

--- src/modules/base/db/__init__.py
+++ src/modules/base/db/__init__.py
@@ -1,7 +1,18 @@
+""" Import the required modules """
+from .base import (
+    BaseDB,
+    BaseSchemaAuditLog,
+    BaseSchemaAuditLogDeleteLog,
+    BaseSchemaUUIDAuditLogDeleteLog
+)
 from .session import session, session_factory
 from .transactional import Transactional
 
 __all__ = [
+    "BaseDB",
+    "BaseSchemaAuditLog",
+    "BaseSchemaAuditLogDeleteLog",
+    "BaseSchemaUUIDAuditLogDeleteLog",
     "session",
     "Transactional",
     "session_factory"
